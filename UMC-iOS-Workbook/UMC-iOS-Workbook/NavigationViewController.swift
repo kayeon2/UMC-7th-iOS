b40bce3c3dffb154f3b0e1e5b86b1cd591094cdc
kayeon2 <tjsrkdus@gmail.com>
2024-10-12 23:10:59 +0900

    TabBarController 구현

---
+++ UMC-iOS-Workbook/UMC-iOS-Workbook/NavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationViewController.swift
+//  UMC-iOS-Workbook
+//
+//  Created by 선가연 on 10/12/24.
+//
+
+import UIKit
+
+class NavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
