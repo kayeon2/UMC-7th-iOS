f15f5391b73a5d72faf34f451203a1f415df3495
kayeon2 <tjsrkdus@gmail.com>
2024-10-12 23:38:07 +0900

    Navigation 방식으로 화면 전환 구현

--- UMC-iOS-Workbook/UMC-iOS-Workbook/NavigationViewController.swift
+++ UMC-iOS-Workbook/UMC-iOS-Workbook/NavigationViewController.swift
@@ -8,22 +8,42 @@
 import UIKit
 
 class NavigationViewController: UIViewController {
+    private let label = UILabel()
+    private let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        label.text = "버튼을 누르면 네비게이션 방식의 화면 전환이 일어나요"
+        label.textColor = .orange
+        
+        button.setTitle("버튼을 눌러주세요", for: .normal)
+        button.backgroundColor = .systemTeal
+        
+        button.addTarget(self, action: #selector(buttonDidTap), for: .touchUpInside)
+        
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(200)
+            $0.centerX.equalToSuperview()
+        }
+        
+        button.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(40)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(55)
+            $0.width.equalTo(255)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc
+    private func buttonDidTap() {
+        let viewController = UIViewController()
+        
+        viewController.view.backgroundColor = .lightGray
+        
+        navigationController?.pushViewController(viewController, animated: true)
     }
-    */
-
 }
