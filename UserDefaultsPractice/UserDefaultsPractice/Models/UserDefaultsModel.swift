2603d4271137e438357eb9eb098c6d79237651de
kayeon2 <tjsrkdus@gmail.com>
2024-10-29 04:45:59 +0900

    [Week3] Model 작성

--- UserDefaultsPractice/UserDefaultsPractice/Models/UserDefaultsModel.swift
+++ UserDefaultsPractice/UserDefaultsPractice/Models/UserDefaultsModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class UserDefaultsModel {
+    private let userDefaults = UserDefaults.standard
+    private let userTextKey: String = "userText"
+    
+    // 유저가 입력한 텍스트 값 저장
+    public func saveUserText(_ text: String) {
+        userDefaults.set(text, forKey: userTextKey)
+    }
+    
+    // UserDefaults에 저장된 값 반환
+    public func loadUserText() -> String? {
+        return userDefaults.string(forKey: userTextKey)
+    }
+}
