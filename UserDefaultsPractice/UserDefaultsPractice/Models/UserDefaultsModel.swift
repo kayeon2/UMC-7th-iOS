6c277e3a6de10625693d8363b2a88bd3c792b904
kayeon2 <tjsrkdus@gmail.com>
2024-10-29 04:41:13 +0900

    [Week3] 파일 및 폴더 생성

---
+++ UserDefaultsPractice/UserDefaultsPractice/Models/UserDefaultsModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsModel.swift
+//  UserDefaultsPractice
+//
+//  Created by 선가연 on 10/29/24.
+//
+
+import Foundation
---
+++ UserDefaultsPractice/UserDefaultsPractice/Views/UserDefaultsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaultsView.swift
+//  UserDefaultsPractice
+//
+//  Created by 선가연 on 10/29/24.
+//
+
+import UIKit
+
+class UserDefaultsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
