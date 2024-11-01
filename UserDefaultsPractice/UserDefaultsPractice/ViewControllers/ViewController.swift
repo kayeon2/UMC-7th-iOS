5b0e51b6b677ecd9713bfa9cc33ea88fa6f2a3fa
kayeon2 <tjsrkdus@gmail.com>
2024-11-02 01:07:28 +0900

    [Week3] ViewController 작성

--- UserDefaultsPractice/UserDefaultsPractice/ViewControllers/ViewController.swift
+++ UserDefaultsPractice/UserDefaultsPractice/ViewControllers/ViewController.swift
@@ -9,15 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let userDefaultsModel = UserDefaultsModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = userDefaultsview
+        loadTextValue()
     }
     
     private lazy var userDefaultsview: UserDefaultsView = {
         let view = UserDefaultsView()
+        view.saveButton.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
         return view
     }()
-
+    
+    @objc func saveButtonClicked() {
+        guard let text = userDefaultsview.inputTextField.text, !text.isEmpty else {
+            return
+        }
+        
+        // 텍스트 저장
+        userDefaultsModel.saveUserText(text)
+        
+        // 저장된 텍스트를 레이블에 표시
+        userDefaultsview.showTextValue.text = text
+    }
+    
+    // 저장된 텍스트 값이 있을 경우 가져오기
+    private func loadTextValue() {
+        if let savedText = userDefaultsModel.loadUserText() {
+            userDefaultsview.showTextValue.text = savedText
+        }
+    }
 }
-
