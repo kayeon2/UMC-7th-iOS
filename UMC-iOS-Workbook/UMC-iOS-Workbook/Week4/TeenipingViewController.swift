9d891912697f829660555898cdbfafb2d1696662
kayeon2 <tjsrkdus@gmail.com>
2024-11-02 01:50:31 +0900

    [Week4] UISegmentedControl 인덱스 업데이트 시 실행될 메서드 연결

--- UMC-iOS-Workbook/UMC-iOS-Workbook/Week4/TeenipingViewController.swift
+++ UMC-iOS-Workbook/UMC-iOS-Workbook/Week4/TeenipingViewController.swift
@@ -15,5 +15,17 @@ class TeenipingViewController: UIViewController {
         super.viewDidLoad()
         self.view = rootView
     }
-
+    
+    private func setupAction() {
+        rootView.segmentedControl.addTarget(
+            self,
+            action: #selector(segmentedControlValueChanged(segment:)),
+            for: .valueChanged
+        )
+    }
+    
+    @objc
+    private func segmentedControlValueChanged(segment: UISegmentedControl) {
+        // TODO: segment 인덱스에 따라 collectionview 표시 여부 결정
+    }
 }
