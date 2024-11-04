f1bec3f183d412fa4e269c639f8238cef87697d0
kayeon2 <tjsrkdus@gmail.com>
2024-11-05 00:05:35 +0900

    [Week5] Rainbow 파일 생성 및 모델 작성

---
+++ UMC-iOS-Workbook/UMC-iOS-Workbook/Week5/RainbowView.swift
@@ -0,0 +1,20 @@
+//
+//  RainbowView.swift
+//  UMC-iOS-Workbook
+//
+//  Created by 선가연 on 11/4/24.
+//
+
+import UIKit
+
+class RainbowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
